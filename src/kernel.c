867fa78ea9d7189a23fd4da901bf385aa7ede161
Deepak Das <email.deepakdas@gmail.com>
2020-05-25 19:04:45 +0530

    call uart init and print Hello World
    Signed-off-by: Deepak Das <email.deepakdas@gmail.com>

--- src/kernel.c
+++ src/kernel.c
@@ -1,8 +1,14 @@
 #include "gpio_led.h"
 #include "utils.h"
+#include "uart.h"
 
 void kernel_main(void)
 {
 	gpio_led_init();
 	gpio_led_on();
+	uart_init();
+	uart_send_string("Hello, world!\n");
+	while (1) {
+		uart_send(uart_recv());
+	}
 }
