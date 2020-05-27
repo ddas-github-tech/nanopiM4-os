9d01a3c4d96c421da539400dde4767ddcb16dafc
Deepak Das <email.deepakdas@gmail.com>
2020-05-27 10:43:19 +0530

    kernel: print current exception level
    this commit adds current exception level print called from
    main funciton.
    
    Signed-off-by: Deepak Das <email.deepakdas@gmail.com>

--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,14 @@
 #include "gpio_led.h"
+#include "printf.h"
 #include "utils.h"
 #include "uart.h"
 
 void kernel_main(void)
 {
+	int el;
+	init_printf(0, putc);
+	el = get_el();
+	printf("Exception level: %d \r\n", el);
 	gpio_led_init();
 	gpio_led_on();
 	uart_init();
