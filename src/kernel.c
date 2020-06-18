25011d5a8139e2a69dca2388ffe9c63b3b871040
Deepak Das <email.deepakdas@gmail.com>
2020-06-18 14:37:02 +0530

    kernel: initialize and test interrupt
    this commit initializes GIC and test the interrupts using dummy IRQ.
    
    Signed-off-by: Deepak Das <email.deepakdas@gmail.com>

--- src/kernel.c
+++ src/kernel.c
@@ -2,17 +2,26 @@
 #include "printf.h"
 #include "utils.h"
 #include "uart.h"
+#include "gic.h"
+#include "irq.h"
+#include "irq_test.h"
 
 void kernel_main(void)
 {
 	int el;
-	init_printf(0, putc);
-	el = get_el();
-	printf("Exception level: %d \r\n", el);
 	gpio_led_init();
 	gpio_led_on();
 	uart_init();
 	uart_send_string("Hello, world!\n");
+	init_printf(0, putc);
+	el = get_el();
+	printf("Hello World\n");
+	printf("Exception level: %d \r\n", el);
+	irq_vector_init();
+	gic_init();
+	test_irq();
+	unmask_irq();
+
 	while (1) {
 		uart_send(uart_recv());
 	}
