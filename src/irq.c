c93028b3a4830bc087c6e9e6332c51614fde78d0
Deepak Das <email.deepakdas@gmail.com>
2020-06-18 12:50:08 +0530

    irq: implement base exception handlers
    This commit implements base handlers for exceptions.
    base handler for interrupt will call the registered irq handler
    and acknowledge it.
    base handler for other execption will print the exception type
    along with ESR and address for the instruction.
    
    also provides macro to mask/unamsk_irq and evt intialization
    which can be called from main file.
    
    Signed-off-by: Deepak Das <email.deepakdas@gmail.com>

---
+++ src/irq.c
@@ -0,0 +1,58 @@
+#include "printf.h"
+#include "entry.h"
+#include "irq.h"
+
+/* array of funcion pointers to hold handlers */
+void (*call_handler[MAX_SPI])(void *) = { 0UL };
+
+/* array of void * to pass args to handlers */
+void* handler_args[MAX_SPI] = {  0UL };
+
+const char *entry_error_messages[] = {
+	"SYNC_INVALID_EL1t",
+	"IRQ_INVALID_EL1t",		
+	"FIQ_INVALID_EL1t",		
+	"ERROR_INVALID_EL1T",		
+
+	"SYNC_INVALID_EL1h",		
+	"IRQ_INVALID_EL1h",		
+	"FIQ_INVALID_EL1h",		
+	"ERROR_INVALID_EL1h",		
+
+	"SYNC_INVALID_EL0_64",		
+	"IRQ_INVALID_EL0_64",		
+	"FIQ_INVALID_EL0_64",		
+	"ERROR_INVALID_EL0_64",	
+
+	"SYNC_INVALID_EL0_32",		
+	"IRQ_INVALID_EL0_32",		
+	"FIQ_INVALID_EL0_32",		
+	"ERROR_INVALID_EL0_32"	
+};
+
+void show_invalid_entry_message(int type, unsigned long esr, unsigned long address)
+{
+	printf("%s, ESR: %x, address: %x\r\n", entry_error_messages[type], esr, address);
+}
+
+void handle_irq(void)
+{
+	unsigned int irq;
+
+	irq = get_pending_interrupt();
+
+	printf("pending irq:%d \r\n", irq);
+	if (call_handler[irq - SPI_OFFSET])
+		call_handler[irq - SPI_OFFSET](handler_args[irq - SPI_OFFSET]);
+	else
+		printf("Unknown pending irq: \r\n");
+
+	deactivate_interrupt(irq);
+}
+
+void request_irq( unsigned int irq_no, irq_type_t type, void (* irq_handler)( void* ), void* args )
+{
+	call_handler[irq_no - SPI_OFFSET] = irq_handler;
+	handler_args[irq_no - SPI_OFFSET] = args;
+	enable_irq(irq_no, type);
+}
