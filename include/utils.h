15692e81feb6d46c0198855f77455695d1b30e20
Deepak Das <email.deepakdas@gmail.com>
2020-06-18 13:11:26 +0530

    utils: add macro to read/write 64bit memaddress
    this commit adds macro helper function to read/write 64 bit
    memory address.
    
    Signed-off-by: Deepak Das <email.deepakdas@gmail.com>

--- include/utils.h
+++ include/utils.h
@@ -1,8 +1,11 @@
 #ifndef	_BOOT_H
 #define	_BOOT_H
+#include <stdint.h>
 
 extern void put32 ( unsigned long, unsigned int );
 extern unsigned int get32 ( unsigned long );
+extern void put64 ( uint64_t, uint64_t );
+extern uint64_t get64 ( uint64_t );
 extern unsigned int get_el ( void );
 
 #endif  /*_BOOT_H */
